5b34895dc5acb8651d36d565ec45d713370a2c05
crossopt <s17b2_fedorkina@179.ru>
2017-04-06 16:13:44 +0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -2,12 +2,8 @@ import random as rd #i am the best translator ever!
 
 
 cluedo_open = [0, 0, 0, 6, 6, 3, 6] #[0, 0, 0, 0, 2, 3, 0]
-cluedo_people = ["Miss Scarlett", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Colonel Mustard", "Mrs. White"]
-cluedo_weapons = ["Candlestick", "Dagger", "Lead pipe", "Revolver", "Rope", "Wrench"]
-cluedo_places = ["Kitchen", "Ballroom", "Conservatory", "Dining room", "Billiard room", "Library", "Lounge", "Hall", "Study"]
 
-
-class CluedoDeck:
+class deck:
 	def __init__(self, ppl = [], wps = [], pls = []):
 		self.people = ppl
 		self.weapons = ["with a " + i for i in wps] #very bad!
@@ -16,22 +12,22 @@ class CluedoDeck:
 	def get(self):
 		return self.people, self.weapons, self.places
 
-def cluedo_init():
-    global cluedo_people, cluedo_places, cluedo_weapons
-    people = ["Miss Scarlett", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Colonel Mustard", "Mrs. White"]
-    places = ["Kitchen", "Ballroom", "Conservatory", "Dining room", "Billiard room", "Library", "Lounge", "Hall", "Study"]
-    weapons = ["Candlestick", "Dagger", "Lead pipe", "Revolver", "Rope", "Wrench"]
-    normal_deck = CluedoDeck(people, weapons, places)
 
-    people = ["Draco Malfoy", "Bellatrix Lestrange", "Dolores Umbridge", "Peter Pettigrew", "Crabbe & Goyle", "Grindelwald"]
-    places = ["Owlery", "Trophy room", "Library", "DADA classroom", "Great Hall", "Hospital wing", "Room of Requirements", "Potions classroom", "Divination classroom"]
-    weapons = ["Sleeping draught", "Cursed necklace", "Petrificus totalus", "Crucio", "Avada Kedavra", "Vanishing cabinet"]
-    hp_deck = CluedoDeck(people, weapons, places)
+people = ["Miss Scarlett", "Professor Plum", "Mrs. Peacock", "Mr. Green", "Colonel Mustard", "Mrs. White"]
+places = ["kitchen", "ballroom", "conservatory", "dining room", "billiard room", "library", "lounge", "hall", "study"]
+weapons = ["candlestick", "dagger", "lead pipe", "revolver", "rope", "wrench"]
+normal_deck = deck(people, weapons, places)
 
-    people = ["Vasechka #1", "Vasechka #2", "Vasechka #3", "Vasechka #4", "Vaseckha #5", "Vaseckha #6", "Vaseckha #7", "Vaseckha #8", "Vaseckha #9"]
-    places = ["209 classroom", "216 classroom", "Corridor", "St. Petersburg", "Kazan", "MSU"]
-    weapons = ["Keyboard", "Mouse", "Big, big, birch", "Broken heart", "Dumb memes", "Very big birch"]
-    vasechka_deck = CluedoDeck(people, weapons, places)
+people = ["Draco Malfoy", "Bellatrix Lestrange", "Dolores Umbridge", "Peter Pettigrew", "Crabbe & Goyle", "Grindelwald"]
+places = ["Owlery", "Trophy room", "library", "DADA classroom", "Great Hall", "hospital wing", "Room of Requirements", "Potions classroom", "Divination classroom"]
+weapons = ["Bat-Bogey hex", "Stupefy", "Petrificus totalus", "Cruciatus curse", "Killing curse", "Confundus charm"]
+hp_deck = deck(people, weapons, places)
 
-    decks = [normal_deck, hp_deck, vasechka_deck]
-    cluedo_people, cluedo_weapons, cluedo_places = rd.choice(decks).get()
+people = ["Vasechka #1", "Vasechka #2", "Vasechka #3", "Vasechka #4", "Vaseckha #5", "Vaseckha #6", "Vaseckha #7", "Vaseckha #8", "Vaseckha #9"]
+places = ["SPBAU", "ITMO", "MSU", "HSE", "MIPT", "middle of nowhere"]
+weapons = ["Dijkstra algorithm", "DFS", "big, big treap", "CENSORED", "suffix tree", "very big segment tree"]
+vasechka_deck = deck(people, weapons, places)
+
+decks = [normal_deck, hp_deck, vasechka_deck]
+cluedo_people, cluedo_weapons, cluedo_places = rd.choice(decks).get()
+print(rd.choice(decks).get())

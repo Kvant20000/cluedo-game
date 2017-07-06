831bf2274055ffa8fa10eccf2119ff53441a0503
Anton <galilei2000@users.noreply.github.com>
2017-07-06 22:09:15 +0300

    Update bang_main.py

--- bang_main.py
+++ bang_main.py
@@ -18,9 +18,19 @@ MAX_GAMES = 10
 bot = telebot.TeleBot(TOKEN_2)
 ###
 
+#@bot.message_handler()
+def allasd(message):
+    print(message.text)
+    return    
+    
+
+game = None
+
 AdminId = [186898465, 319325008]
-Admins = [telebot.types.User(id=186898465, username='TwoBlueCats', first_name='Anton', last_name='Anikushin'),
-          telebot.types.User(id=319325008, username='greatkorn', first_name='Anton', last_name='Kvasha')]
+Admins = [
+          telebot.types.User(id=186898465, username='TwoBlueCats', first_name='Anton', last_name='Anikushin'),
+          #telebot.types.User(id=319325008, username='greatkorn', first_name='Anton', last_name='Kvasha')
+          ]
 
 common_cards = ["bang", "miss", "beer", "saloon", "shop", "draw", "panic", "tnn", "duel", "gatling", "indeans"]
 special_cards = ["weapon", "mustang", "zoom", "barrel", "prison", "dynamite"]
@@ -91,25 +101,28 @@ class Player:
         self.cards.append(card)
 
     def delCard(self, card):
-        used.push_back(self.cards[self.cards.index(card)])
+        gm = getGame(self)
+        gm.used.append(self.cards[self.cards.index(card)])
         self.cards.pop(self.cards.index(card))
 
     def addActive(self, cards):
         self.active.append(card)
 
     def delActive(self, cards):
-        used.push_back(self.cards[self.cards.index(card)])
+        gm = getGame(self)
+        gm.used.append(self.cards[self.cards.index(card)])
         self.active.pop(self.active.index(card))
 
     def cardsInHand(self):
         return ', '.join(self.cards)
 
     def draw_card(self):
-        if len(deck) == 0:
-            deck = used
+        gm = getGame(self)
+        if len(gm.deck) == 0:
+            gm.deck = used
             rd.shuffle(deck)
-        self.addCard(deck[0])
-        deck.pop(0)
+        self.addCard(gm.deck[0])
+        gm.deck.pop(0)
 
 
 class Game:
@@ -120,11 +133,13 @@ class Game:
         self.started = False
         self.end = False
         self.now = 0
-
+        
+        self.curr_card = self.curr_ans = self.curr_person = ""
+        
         self.players = []
 
 
-    def add_player(self, P):
+    def add_player(self, p):
         self.players += [p]
 
     def start(self):
@@ -147,7 +162,7 @@ class Game:
                 self.now = i
                 pl.mx_hp += 1
                 pl.hp += 1
-            pl.setCards(deck[last:last + pl.mx_hp])
+            pl.setCards(self.deck[last:last + pl.mx_hp])
             last += pl.mx_hp
 
     def game(self):
@@ -164,57 +179,92 @@ class Game:
             if str(pl) == name:
                 return pl
     
-    def check_death(person):
+    def check_death(self, person):
         if person.hp == 0 and "beer" in person.cards():
-            send_message(person.id, "You must use beer to stay alive")
+            bot.send_message(person.id, "You must use beer to stay alive")
             person.hp += 1
             person.delCard("beer")
             return False
         else:
             return True
-
-    def bang(person1):
-        send_message(person1.id, "You can shoot at: ", reply_markup = make(playersList(self)))
+    
+    def bang(self, person1):
+        bot.send_message(person1.id, "You can shoot at: ", reply_markup = make(playersList(self) + ["Cancel"]))
         while self.curr_person == "":
             pass
-            
+        
+        if self.curr_person == "Cancel":
+            self.curr_person = ""
+            return
+        
         person2 = self.getPlayer(self.curr_person)
         self.curr_person = ""
-        if "miss" in person2.cards():
-            send_message(person2.id, "You have {0} hp. Do you want to use miss?".format(person2.hp))
+        
+        if "miss" in person2.cards:
+            bot.send_message(person2.id, "You have {0} hp. Do you want to use miss?".format(person2.hp))
             bot.send_message(person2.id, "Choose yes/no: ", reply_markup = make(["Yes", "No"]))
             while self.curr_ans == "":
                 pass
             if self.curr_ans == "Yes":
                 person2.delCard("miss")
-            else
+            else:
                 person2.hp -= 1
-                if check_death(person2):
+                if self.check_death(person2):
                     person2.status = 0
             self.curr_ans = ""
         else:
             person2.hp -= 1
-            if check_death(person2):
+            if self.check_death(person2):
                 person2.status = 0
         person1.delCard("bang")
-
+        
+        
+    def beer(self, person1):
+        if person1.hp < person1.mx_hp:
+            bot.send_message(person1.id, "You drink beer and regenerate one hit point")
+            person1.hp += 1
+            person1.delCard("beer")
+        else:
+            bot.send_message(person1.id, "You have maximum hit points")
+        return
+    
+    def tmp(self, person1):
+        return
+    
+    def miss(self, person1):
+        return
+    
     def turn(self):
         print("Now it is player № " + str(self.now) + ' turn')
-        P = players[self.now]
+        P = self.players[self.now]
         P.draw_card()
         P.draw_card()
-
-        send_message(P.id, "Cards in you hand: ", reply_markup = make(P.cards))
-        while self.curr_card == "":
-            pass
-        tmp = self.curr_card
-        self.curr_card = ""
-        exec("self." + tmp + "(P)")
-
+        
+        tmp = "tmp"
+        while tmp != "End":
+            exec("self." + tmp + "(P)")
+            bot.send_message(P.id, "Cards in your hand: ", reply_markup = make(P.cards + ["End"]))
+            while self.curr_card == "":
+                pass
+                
+            tmp = self.curr_card
+            self.curr_card = ""
+        
+        num_cards = len(P.cards)
+        if num_cards > P.hp:
+            bot.send_message(P.id, "You must fold " + str(num_cards - P.hp))
+            for i in range(num_cards - P.hp):
+                bot.send_message(P.id, "Choose cards from your hand: ", reply_markup = make(P.cards))
+                while self.curr_card == "":
+                    pass
+                P.delCard(self.curr_card)
+                self.curr_card = ""
+        
         return False
         #'return True' if game is finished
 
-@bot.message_handler(command=['join'])
+
+@bot.message_handler(commands=['join'])
 def addPlayer(message):
     global game
     pl = Player(message.from_user)
@@ -228,11 +278,12 @@ def addPlayer(message):
     personToGame[pl] = game
     game.add_player(pl)
     bot.send_message(pl.id, "you join the game")
-    send_all(str(pl) + ' join our game', bad = [pl])
-
+    sendAll(str(pl) + ' join our game', bad = [pl])
     
-@bot.message_handler(command=['game'])
+    
+@bot.message_handler(commands=['game'])
 def startGame(message):
+    print(message.text)
     sendAdmin("pre-game init")
     game.start()
     sendAdmin("main loop starts")
@@ -242,18 +293,25 @@ def startGame(message):
 
 @bot.message_handler(func=lambda message : messageType(message) == "card")
 def chooseCard(message):
+    print(message.text)
     gm = getGame(message)
     gm.curr_card = message.text
     
 @bot.message_handler(func=lambda message : messageType(message) == "person")
 def choosePerson(message):
+    print(message.text)
     gm = getGame(message)
     gm.curr_person = message.text
 
 @bot.message_handler(func=lambda message : messageType(message) == "binary")
 def chooseCard(message):
+    print(message.text)
     gm = getGame(message)
     gm.curr_ans = message.text
+    
+@bot.message_handler()
+def catcher(message):
+    print(message.from_user.id, message.text)
     
 def messageType(message):
     gm = getGame(message)
@@ -261,9 +319,9 @@ def messageType(message):
         return "ignore"
     
     text = message.text
-    if text in ["bang", "miss", "beer"]:
+    if text in ["bang", "miss", "beer", "End"]:
         return "card"
-    if text in playersList(gm):
+    if text in playersList(gm) + ["Cancel"]:
         return "person"
     if text in ["Yes", "No"]:
         return "binary"
@@ -283,7 +341,7 @@ def getGame(User):#raise 'TypeError'
     
 def sendAll(msg, bad=[]):
     for player in game.players:
-        if player.id not in bad:
+        if player not in bad:
             bot.send_message(player.id, msg)
     return
 
@@ -350,6 +408,8 @@ def main():
     sendAdmin('Bot starts')
     bot.polling()
 
+
+    
 if __name__ == '__main__':
     main()
 
